fe845b04f944f32159e4228c763d8c7861b4c138
Alin Merchant <amer0024@student.monash.edu>
2025-07-22 21:39:52 +1000

    configuration

---
+++ db/db_config.py
@@ -0,0 +1,13 @@
+import mysql.connector
+import uuid
+from datetime import datetime
+
+config = {
+    "host": "gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
+    "user": "2vd4KSQmyUgoYAj.root",
+    "password": "exasJpFFhvhs5DGf",
+    "database": "test",
+    "port": 4000,
+    "ssl_ca": "C:\Users\Alin Merchant\OneDrive\Desktop\CrisisMapper-AI\CrisisMapper-AI\db\isrgrootx1.pem",
+    "ssl_verify_cert": True
+}
